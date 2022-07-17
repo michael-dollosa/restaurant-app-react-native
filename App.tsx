0ba9b13b4341b7518efebdb058c949a4b0bbedde
Michael Dollosa <dollosa.christian@gmail.com>
2022-07-17 14:51:08 +0800

    installed init packages and components

--- App.tsx
+++ App.tsx
@@ -1,20 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Platform,
+  StatusBar,
+} from "react-native";
+import { Searchbar } from "react-native-paper";
+import { RestaurantsScreens } from "./src/features/restaurants/screens/restaurants.screens";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <RestaurantsScreens />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
